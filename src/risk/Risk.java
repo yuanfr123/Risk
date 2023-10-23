bcdad78761210003feb2007bbd3be386ccb15328
yalouini <yalouini07@gmail.com>
2023-10-23 12:16:57 +0200

    test lisa
    sysout pour test lisa

--- src/risk/Risk.java
+++ src/risk/Risk.java
@@ -5,7 +5,8 @@ public class Risk {
 	public static void main(String[] args) {
 		System.out.println("Le Risk fonctionne correctement");
 		System.out.println("Oui en effet");
-
+		System.out.println("Sur");
+		System.out.println("test lisa");
 	}
 
 }

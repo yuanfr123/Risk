812816bf1cb6747fec904b283de672240d53c4ef
lisakouly <lisakoulytchizky@yahoo.fr>
2023-10-23 16:31:44 +0200

    Ajout de la classe Joueur
    Ajoute de la classe joueur
    Getter setter attribut constructeur

--- src/risk/Risk.java
+++ src/risk/Risk.java
@@ -1,7 +1,6 @@
 package risk;
 
 public class Risk {
-
 	public static void main(String[] args) {
 		System.out.println("Le Risk fonctionne correctement");
 		System.out.println("Oui en effet");
--- src/risk/model/Joueur.java
+++ src/risk/model/Joueur.java
@@ -1,5 +1,53 @@
 package risk.model;
+import java.util.Date;
 
 public class Joueur {
+	// Attributs 
+	private int id; 
+	private String nom;
+	private String prenom;
+	private Date dtNaissance = new Date();
+	private String statut;
 
+	// Constructeur
+	/**
+	 * @param id
+	 */
+	public Joueur(int id) {
+		this.id = id;
+	}
+	
+	// Methodes
+	/** @return int */
+	public int getId() {
+		return id;
+	}
+	/** @param id */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/** @return String */
+	public String getNom() {
+		return nom;
+	}
+	/** @param nom */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/** @return String */
+	public String getPrenom() {
+		return prenom;
+	}
+	/** @param prenom */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/** @return Date */
+	public Date getDtNaissance() {
+		return dtNaissance;
+	}
+	/** @param dtNaissance */
+	public void setDtNaissance(Date dtNaissance) {
+		this.dtNaissance = dtNaissance;
+	}	
 }

5eaabda1ee10d71b1897103abc5c6e45d358cc76
sujedr <dejus.raphael@gmail.com>
2023-10-23 16:17:55 +0200

    création fenêtre jeu + javadoc globale

--- src/risk/Risk.java
+++ src/risk/Risk.java
@@ -1,11 +1,22 @@
 package risk;
 
+import risk.vue.Fenetre;
+
+/**
+ * 
+ */
 public class Risk {
 
+	/**
+	 * Classe principale du projet
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Le Risk fonctionne correctement");
 		System.out.println("Oui en effet");
 		System.out.println("Sur");
-		System.out.println("HAhahahaha");
+		
+        new Fenetre(); // Crée une instance de Fenetre
+
 	}
 }

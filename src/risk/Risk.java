c386bdbfcdf919a2863daedb69008f1d23f7940d
sujedr <dejus.raphael@gmail.com>
2023-10-24 14:56:28 +0200

    ajout du projet local

--- src/risk/Risk.java
+++ src/risk/Risk.java
@@ -12,11 +12,6 @@ public class Risk {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Le Risk fonctionne correctement");
-		System.out.println("Oui en effet");
-		System.out.println("Sur");
-		
-        new Fenetre(); // Crée une instance de Fenetre
-
+        Fenetre vue = new Fenetre(); // Crée une instance de Fenetre
 	}
 }
